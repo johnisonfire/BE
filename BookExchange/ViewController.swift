e86efb2a08b6c0e5403b0810bc251a79875ae71c
Christopher John Ison <john@christohersmini.attlocal.net>
2018-03-25 22:04:43 -0400

    First Commit

---
+++ BookExchange/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookExchange
+//
+//  Created by Christopher John Ison on 3/24/18.
+//  Copyright © 2018 Christopher John Ison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
